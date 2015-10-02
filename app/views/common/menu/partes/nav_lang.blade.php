7454b68f0de63254818110362eccee226ec3b571
Willy <willy_maciel@live.com>
2015-10-02 20:45:12 -0300

    Adicionado novas bandeiras

--- app/views/common/menu/partes/nav_lang.blade.php
+++ app/views/common/menu/partes/nav_lang.blade.php
@@ -1,14 +1,14 @@
 <li>
         @if(Config::get('app.locale') == 'pt')
             <a href="{{URL::to('lang?lang=Portuguese')}}">
-            <img src="images/icon/flags/pt-br.png">
+            <img src="images/icon/flags/pt-br.jpg">
             {? $extra_lang = 'English' ?}
-            {? $extra_lang_img = '<img src="images/icon/flags/en-us.png">' ?}
+            {? $extra_lang_img = '<img src="images/icon/flags/en-us.jpg">' ?}
         @else
             <a href="{{URL::to('lang?lang=English')}}">
-            <img src="images/icon/flags/en-us.png">
+            <img src="images/icon/flags/en-us.jpg">
             {? $extra_lang = 'Portuguese' ?}
-            {? $extra_lang_img = '<img src="images/icon/flags/pt-br.png">' ?}
+            {? $extra_lang_img = '<img src="images/icon/flags/pt-br.jpg">' ?}
         @endif
     </a>
 </li>
